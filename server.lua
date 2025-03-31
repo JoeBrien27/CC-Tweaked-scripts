2ed03d06aff5e57181bbc711fa22b1feba603c04
JoeBrien27 <69897295+JoeBrien27@users.noreply.github.com>
2025-03-31 18:32:21 +0100

    fixed poor formating of output string

--- server.lua
+++ server.lua
@@ -5,9 +5,8 @@ modem.open(80)
 local event, side, channel, replyChannel, message, distance
 
 while true do
-	
 	repeat
 		event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
 	until channel == 80
-		print(tostring("message:", message, "\n Channel:", replyChannel)
+	print("message:", tostring(message), "\n Channel:", tostring(replyChannel))
 end
